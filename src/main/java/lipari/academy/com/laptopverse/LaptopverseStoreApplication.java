25ab7a97aa64f070e748a121a94d3411de1c79a1
Teshale C. <teshale.cella@liparipeople.com>
2026-04-17 18:27:41 +0200

    init: Progetto Base Spring Boot

---
+++ src/main/java/lipari/academy/com/laptopverse/LaptopverseStoreApplication.java
@@ -0,0 +1,13 @@
+package lipari.academy.com.laptopverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopverseStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopverseStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lipari/academy/com/laptopverse/LaptopverseStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package lipari.academy.com.laptopverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopverseStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
